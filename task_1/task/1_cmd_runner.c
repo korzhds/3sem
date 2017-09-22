871f5b014d6478f14763a7d49be90778cab5a069
korzhds <korzh.ds@phystech.edu>
2017-09-23 01:06:05 +0300

    HW for 27_09

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -1,11 +1,60 @@
-/*
-  Write a program which allow you to: 
-- run another programs via command line.
-- get exit codes of terminated programs
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <malloc.h>
 
-## TIPS:
-1. Use "2_fork_wait_exit.c" and "4_exec_dir.c" from examples. Combine them.
-2. Parse input string according to the type of exec* (see "man exec").
-   a) if execvp is used, string splitting into "path" and "args" is all you need.
-3. Collect exit codes via waitpid/WEXITSTATUS.
-*/
+
+int main() {
+  char ** a;
+  char s[256] = "";
+  pid_t pid;
+  printf("Enter argumets or 'quit' for exit\n");
+  fgets(s, 255, stdin);
+  int status;
+  while(strcmp(s, "quit\n") != 0) {
+    int k = 0;
+    int j = 0, i = 0;
+    a = (char **) malloc(sizeof(char *));
+    while (s[i] != '\n') {
+      a = (char **) realloc(a, (k + 1) * sizeof(char *));
+      a[k] = (char *) malloc(256);
+      j = 0;
+      while (s[i] == ' ')
+        i++;
+      while ((s[i] != ' ') && (s[i] != '\n')) {
+        a[k][j] = s[i];
+        i++;
+        j++;
+      }
+      a[k][j] = '\0';
+      k++;
+    }
+    a = (char **) realloc(a, (k + 1) * sizeof(char *));
+    a[k] = NULL;
+    switch(pid = fork()) {
+      case 0:
+        execv(a[0], a);
+        printf("Error in execv\n");
+        exit(2);
+        break;
+      case -1:
+        printf("error in fork\n");
+        exit(42);
+        break;
+      default:
+        waitpid(pid, &status, 0);
+        printf("Ret code: %d\n", WEXITSTATUS(status));
+    }
+    for (int m = 0; m < k; m++) {
+      free(a[m]);
+    }
+    free(a);
+    printf("Enter argumets or 'quit' for exit\n");
+    fgets(s, 255, stdin);
+  }
+  return 0;
+}
